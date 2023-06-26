8928f95871aa6fcb0c12dd5d625b0c9b5621d815
LorenzoPuliti <135641133+LorenzoPuliti@users.noreply.github.com>
2023-06-26 22:51:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+/* Domanda 1 */
+
+/* in parole semplici un algoritmo è una serie finita di operazioni da svolgere in sequenza per risolvere un problema, per ottenere un preciso risultato
+partendo da un certo numero di dati */
+
+
+/* Domanda 2 */
+
+/* Una variabile è una sorta di scatola alla quale attribuiamo un nome e generalmente pure un valore per memorizzarlo e utilizzarlo in secondo momento.
+E' come se riservassimo una parte di memoria con quel nome e valore e la teniamo da parte */
+
+
+/* domanda 3 */ 
+
+/* Undefined e null non sono la stessa cosa, null indica che il contenuto della variabile è VUOTO,  mentre undefined definisce semplicemente che non è 
+ancora stato definito, di conseguenza il suo contenuto potrebbe essere qualsiasi cosa, undefined */
